bb36e5679c1b35e95fbf7eec0b9fe0704bd7bc30
Himageo2006 <himageo2@gmail.com>
2026-06-12 15:13:34 +0300

    Textbook explain prompts: full lesson, 1200-word minimum, no clarifying questions (v164)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // OstazAI Service Worker v3.0
 // Version bump forces cache refresh on all clients
-const CACHE_VERSION = 'ostazai-v123';
+const CACHE_VERSION = 'ostazai-v124';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
