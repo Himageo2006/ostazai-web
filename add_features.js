3d076e606391bc62984a03ae3c5af640d351008e
Himageo2006 <himageo2@gmail.com>
2026-06-18 19:49:21 +0300

    Untrack dev scratch files; add .gitignore

--- add_features.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const toAppend = fs.readFileSync('features_append.txt', 'utf8');
-let code = fs.readFileSync('app.js', 'utf8');
-code += toAppend;
-fs.writeFileSync('app.js', code);
-console.log('Appended', toAppend.length, 'chars. Total:', code.length);
